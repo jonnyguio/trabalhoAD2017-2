30565fc4298fcaba813ba9fd2bbd55f0e9520b65
Joao Guio <joao.guio@corp.globo.com>
2017-11-21 19:23:35 -0200

    juntando com o Mateus

---
+++ main.py
@@ -0,0 +1,66 @@
+# main.py
+
+# -*- coding: utf-8 -*-
+import numpy as np
+# import matplotlib.pyplot as plt
+import collections as clt
+from queues.exp_queue import Queue
+from servers.exp_server import Server
+
+
+def run_system(queue, queue2, server, epochs, time=0.0):
+    busy = 0.0
+    idle = 0.0
+    queue_size = list()
+    est_busy = 0.0
+
+    for idx in range(epochs):
+        if server.is_empty() == True:
+            client_time = queue.pop_fcfs()
+            if client_time == None:
+                queue.add_client()
+                client_time = queue.pop_fcfs()
+                idle += client_time
+                time += client_time
+                busy = server.do_service(time)
+                time += busy
+                est_busy += busy
+            else:
+                busy = server.do_service(time)
+                time += busy
+                est_busy += busy
+        server.update(time)
+        queue.update(time)
+        if idx%1 == 0:
+            queue_size.append(queue.get_len())
+
+    # plt.plot(range(len(queue_size)), queue_size)
+    # plt.show()
+    ro = est_busy/time
+    return ro
+
+
+def simulateMM1(number_it=100000, lamb=1.0, mu=1.0):
+    queue = Queue(lamb=lamb)
+    queue2 = Queue(lamb=lamb)
+    server = Server(mu=mu)
+    ro = run_system(queue, queue2, server, number_it)
+    return ro
+
+def MM1(lamb=1.0, mu=1.0):
+    ro = lamb/mu
+    X = 1/mu
+    Nq = ro/(1-ro)
+    W = Nq*lamb
+    return dict(zip(
+        ["W", "Nq", "X", "ro"],
+        [W, Nq, X, ro]
+    ))
+
+if __name__ == '__main__':
+    lamb = 0.8
+    mu = 1.0
+    number_it = 100000
+    ro = simulateMM1(number_it, lamb)
+    params = MM1(lamb, mu)
+    print(ro, params["ro"])
---
+++ servers/exp_server.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+class Server(object):
+	"""docstring for Server"""
+	def __init__(self, mu=1.0, start_time=0.0):
+		super(Server, self).__init__()
+		self.__params = self.make_params_dict(mu, start_time)
+
+	def make_params_dict(self, mu, start_time):
+		return dict(zip(
+			["is_empty","mu","start_time","service"],
+			[True, mu, start_time, start_time]
+		))
+
+	#Retorna os parâmetros da classe
+	def get_params(self):
+		return self.__params
+
+	#Avisa quando o servitor está ocupado ou não
+	def is_empty(self):
+		return self.__params["is_empty"]
+
+	#Este método executará o serviço e retornará o tempo de serviço
+	def do_service(self, start_time):
+		#Caso o servidor esteja ocupado, não faça o serviço
+		assert self.__params["is_empty"]
+
+		beta = 1./ self.__params["mu"]
+		self.__params["is_empty"] = False
+		self.__params["start_time"] = start_time
+		self.__params["service"] = np.random.exponential(scale=beta, size=None)
+		return self.__params["service"]
+
+	#Dado o tempo atual da simulação, este método atualiza o servidor
+	def update(self, start_time):
+		self.__params["is_empty"] = (start_time - self.__params["start_time"]) >= (self.__params["service"] - 0.0001)
+		self.__params["start_time"] = start_time
