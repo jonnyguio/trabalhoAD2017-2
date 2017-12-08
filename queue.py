585dcabd0193c73c7301caca727431ea09d626e7
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-08 00:17:59 -0200

    removendo os calculos das metricas

--- queue.py
+++ queue.py
@@ -1,19 +1,18 @@
-
 # -*- coding: utf-8 -*-
-import numpy as np
 import collections as clt
 
 
 class Queue(object):
 	"""docstring for Queue"""
-	def __init__(self, lamb=1.0, start_time=0.0):
+	def __init__(self, queue_type="fcfs"):
 		super(Queue, self).__init__()
-		self.__params = self.make_params_dict(lamb, start_time, clt.deque())
+		self.__queue_type = queue_type
+		self.__params = self.make_params_dict(clt.deque())
 
-	def make_params_dict(self, lamb, start_time, deque):
+	def make_params_dict(deque):
 		return dict(zip(
-			["lambda", "beta", "start_time", "deque"],
-			[lamb, 1./lamb, start_time, deque]
+			["deque"],
+			[deque]
 		))
 
 	def get_params(self):
@@ -22,41 +21,12 @@ class Queue(object):
 	def get_len(self):
 		return len(self.__params["deque"])
 
-	def get_time(self):
-		return self.__params["start_time"]
+	def add(self, client):
+		self.__params["deque"].append(client)
 
-	def add_client(self, size=None):
-		time_client = np.random.exponential(scale=self.__params["beta"], size=size)
-		if size is None:
-			self.__params["deque"].append(time_client)
-			self.__params["start_time"] += time_client
-			return time_client
-		else:
-			self.__params["deque"].extend(time_client)
-			return sum(time_client)
-
-	def pop_fcfs(self):
+	def pop(self):
 		if len(self.__params["deque"]) == 0:
 			return None
-		return self.__params["deque"].popleft()
-
-	def pop_lcfs(self):
-		if len(self.__params["deque"]) == 0:
-			return None
+		if self.__queue_type == "fcfs":
+			return self.__params["deque"].popleft()
 		return self.__params["deque"].pop()
-
-	def update(self, time, prot='FCFS'):
-		acc_time = 0.0
-		delta_time = time - self.__params["start_time"]
-
-		assert delta_time >= 0
-
-		while acc_time <= (delta_time-0.0001):
-			acc_time += self.add_client()
-
-		if prot == 'LCFS':
-			self.pop_lcfs()
-		else:
-			self.pop_fcfs()
-
-		self.__params["start_time"] = time
