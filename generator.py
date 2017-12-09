fb48118484491d792a183b22915564f930a22d91
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-09 12:52:57 -0200

    update generator

--- generator.py
+++ generator.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from events import Event, EVENT_TYPE_ARRIVAL, EVENT_TYPE_END_SERVICE_1, EVENT_TYPE_END_SERVICE_2
+from events import Event, EVENT_TYPE_ARRIVAL, EVENT_TYPE_END_SERVICE1, EVENT_TYPE_END_SERVICE2
 
 class Generator:
-    def __init__(self, lamb, mu1, mu2):
-        self.__betha = [1./lamb, 1./mu1]
+    def __init__(self, lamb, mu):
+        self.__betha = [1./lamb, 1./mu]
 
     def arrival_time(self):
         return np.random.exponential(scale=self.__betha[0], size=None)
 
-    def end_service_1_time(self):
-        return np.random.exponential(scale=self.__betha[1], size=None)
-
-    def end_service_2_time(self):
+    def end_service_time(self):
         return np.random.exponential(scale=self.__betha[1], size=None)
     
     def arrival_event(self, time):
@@ -20,7 +17,7 @@ class Generator:
         return Event(time+time_sample, EVENT_TYPE_ARRIVAL)
 
     def end_service_1_event(self, time, client):
-        return Event(time + client.get_service_time_1(), EVENT_TYPE_END_SERVICE_1)
+        return Event(time + client.get_service_time_1(), EVENT_TYPE_END_SERVICE1)
 
     def end_service_2_event(self, time, client):
-        return Event(time + client.get_service_time_2(), EVENT_TYPE_END_SERVICE_2)
+        return Event(time + client.get_service_time_2(), EVENT_TYPE_END_SERVICE2)
