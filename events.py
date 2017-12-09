1d9976df05699b26dbf15760473b08b94216939a
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-09 00:58:47 -0200

    simplificando a classe event para uma tupla com o tempo e o tipo

--- events.py
+++ events.py
@@ -5,41 +5,16 @@ EVENT_TYPE_END_SERVICE2 = "END_OF_SERVICE2"
 EVENT_TYPE_PREEMPTION = "PREEMPTION"
 
 class Event:
-    def __init__(self, event_data, start_time, n_type):
-        self.data = event_data
-        self.type = n_type
-        self.start_time = start_time
-        self.next = None
+    def __init__(self, start_time, n_type):
+        self.__start_time = start_time
+        self.__type = n_type
     
     #função de comparação para ordenar na heap
     def __lt__(self, other):
-        return self.start_time < other.start_time
-    
-    def set_next(self, n_next):
-        self.next = n_next
-    
-    def get_next(self):
-        return self.next
-
-    def set_event_data(self, n_event_data):
-        self.event_data = n_event_data
-
-    def get_event_data(self):
-        return self.data
+        return self.__start_time < other.__start_time
 
     def set_start_time(self, n_start_time):
-        self.start_time = n_start_time
+        self.__start_time = n_start_time
         
     def get_start_time(self):
-        return self.start_time
-
-    def insert_event(event_list_begin, new_event):
-        if event_list_begin.get_next() is not None:
-            insert_event(event_list_begin.get_next(), new_event)
-        else:
-            event_list_begin.set_next(new_event)
-    
-    def pop_event(event_list_begin):
-        event = event_list_begin
-        event_list_begin = event_list_begin.get_next()
-        return event.data, event.type, event.start_time
+        return self.__start_time
