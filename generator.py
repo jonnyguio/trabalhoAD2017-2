e55ec99894cee89710dde6ed6cbb135a2811a22d
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-09 00:56:06 -0200

    definindo a classe generator

--- generator.py
+++ generator.py
@@ -4,16 +4,23 @@ from events import Event, EVENT_TYPE_ARRIVAL, EVENT_TYPE_END_SERVICE_1, EVENT_TY
 
 class Generator:
     def __init__(self, lamb, mu1, mu2):
-        self.__betha = [1./lamb, 1./mu1, 1./mu2] 
+        self.__betha = [1./lamb, 1./mu1, 1./mu2]
+
+    def arrival_time(self):
+        return np.random.exponential(scale=self.__betha[0], size=None)
+
+    def end_service_1_time(self):
+        return np.random.exponential(scale=self.__betha[1], size=None)
+
+    def end_service_2_time(self):
+        return np.random.exponential(scale=self.__betha[2], size=None)
     
-    def next_arrival_event(time):
-        time_sample = np.random.exponential(scale=self.__betha[0], size=None)
-        return Event({} , time+time_sample, EVENT_TYPE_ARRIVAL)
+    def arrival_event(self, time):
+        time_sample = self.arrival_time()
+        return Event(time+time_sample, EVENT_TYPE_ARRIVAL)
 
-    def next_end_service_1_event(time):
-        time_sample = np.random.exponential(scale=self.__betha[1], size=None)
-        return Event({} , time+time_sample, EVENT_TYPE_ARRIVAL)
+    def end_service_1_event(self, time, client):
+        return Event(time + client.get_service_time_1(), EVENT_TYPE_END_SERVICE_1)
 
-    def next_end_service_2_event(time):
-        time_sample = np.random.exponential(scale=self.__betha[2], size=None)
-        return Event({} , time+time_sample, EVENT_TYPE_ARRIVAL)
+    def end_service_2_event(self, time, client):
+        return Event(time + client.get_service_time_2(), EVENT_TYPE_PREEMPTION)
