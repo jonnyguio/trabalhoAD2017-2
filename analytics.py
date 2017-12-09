9f6ca66a0fcce619efbff5aec4a789b1f3a5e0f1
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-09 16:30:31 -0200

    calculo do W1, T2 e W2

--- analytics.py
+++ analytics.py
@@ -34,6 +34,9 @@ class Analytics():
     def run(self):
         final_metrics = self.__metrics_base.copy()
         final_metrics["E[T1]"] = np.mean([metric["E[T1]"] for metric in self.__metrics])
+        final_metrics["E[W1]"] = np.mean([metric["E[W1]"] for metric in self.__metrics])
+        final_metrics["E[T2]"] = np.mean([metric["E[T2]"] for metric in self.__metrics])
+        final_metrics["E[W2]"] = np.mean([metric["E[W2]"] for metric in self.__metrics])
         self.__final_metrics = final_metrics
         return final_metrics
 
@@ -50,4 +53,7 @@ class Analytics():
     def __new_metric_entry(self):
         new_metrics = self.__metrics_base.copy()
         new_metrics["E[T1]"] = np.mean([client.get_end_service_1() - client.get_start_queue_1() for client in self.clients_list])
+        new_metrics["E[W1]"] = np.mean([client.get_end_service_1() - client.get_start_queue_1() - client.get_service_time_1() for client in self.clients_list])
+        new_metrics["E[T2]"] = np.mean([client.get_end_service_2() - client.get_start_queue_2() for client in self.clients_list])
+        new_metrics["E[W2]"] = np.mean([client.get_end_service_2() - client.get_start_queue_2() - client.get_total_service_time_2() for client in self.clients_list])
         return new_metrics
