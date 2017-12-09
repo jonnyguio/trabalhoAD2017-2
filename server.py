4eeb1e0457ba663b22f4db5efea18c13404ec6eb
Mateus Ildefonso <matecompufrj@gmail.com>
2017-12-09 12:54:09 -0200

    renomear servers para serve e definir os métodos da classe

---
+++ server.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import collections as clt
+
+
+class Server(object):
+	"""docstring for Server"""
+	def __init__(self, maxlen=1):
+		super(Server, self).__init__()
+		self.__params = self.make_params_dict(clt.deque(maxlen=maxlen))
+		self.__params = self.make_params_dict(clt.deque())
+
+	def make_params_dict(self, deque):
+		return dict(zip(
+			["deque"],
+			[deque]
+		))
+
+	def get_params(self):
+		return self.__params
+
+	def get_len(self):
+		return len(self.__params["deque"])
+
+	def is_empty(self):
+		return self.get_len() == 0
+
+	def push(self, client):
+		self.__params["deque"].append(client)
+
+	def pop(self):
+		if len(self.__params["deque"]) == 0:
+			return None
+		return self.__params["deque"].pop()
+
+	def service_type(self):
+		if self.__params["deque"][0].get_start_queue_2() == None:
+			return 1
+		return 2
